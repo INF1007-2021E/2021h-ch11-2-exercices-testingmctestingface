ca0abf227ccd9f79680ba0e536e072793b78b3d0
testingmctestingface <raphael.lheureux@polymtl.ca>
2021-04-13 14:32:36 -0400

    Update magician.py

--- magician.py
+++ magician.py
@@ -5,14 +5,11 @@ Classes pour représenter un magicien et ses pouvoirs magiques.
 """
 
 
-import random
-
-import utils
 from character import *
 
 
 # TODO: Créer la classe Spell qui a les même propriétés que Weapon, mais avec un coût en MP pour l'utiliser
-class Spell:
+class Spell(Weapon):
 	"""
 	Un sort dans le jeu.
 
@@ -21,12 +18,12 @@ class Spell:
 	:param mp_cost: Le coût en MP d'utilisation du sort
 	:param min_level: Le niveau minimal pour l'utiliser
 	"""
-
-	# TODO: __init__
-	pass
+	def __init__(self,name,power,mp_cost,min_level):
+		super().__init__(name,power,min_level)
+		self.mp_cost = mp_cost
 
 # TODO: Déclarer la classe Magician qui étend la classe Character
-class Magician:
+class Magician(Character):
 	"""
 	Un utilisateur de magie dans le jeu. Un magicien peut utiliser des sorts, mais peut aussi utiliser des armes physiques. Sa capacité à utiliser des sorts dépend 
 
@@ -41,18 +38,37 @@ class Magician:
 	:ivar using_magic: Détermine si le magicien tente d'utiliser sa magie dans un combat.
 	"""
 
+
 	def __init__(self, name, max_hp, max_mp, attack, magic_attack, defense, level):
 		# TODO: Initialiser les attributs de Character
 		# TODO: Initialiser le `magic_attack` avec le paramètre, le `max_mp` et `mp` de la même façon que `max_hp` et `hp`, `spell` à None et `using_magic` à False.
-		pass
+		super().__init__(self,name, max_hp, attack, defense, level)
+		self.max_mp = max_mp
+		self.magic_attack = magic_attack
+		self.__mp = max_mp
+		self.using_magic = True
+		self.spell = None
 
 	@property
 	def mp(self):
-		pass
+		return self.__mp
 
 	@mp.setter
 	def mp(self, val):
-		pass
+		if val > self.max_mp:
+			self.mp = self.max_mp
+		else:
+			self.mp = val
+
+	@property
+	def spell(self):
+		return self.__spell
+
+	@spell.setter
+	def spell(self, val):
+		if val is not None and val.min_level > self.level:
+			raise ValueError("level min trop grand")
+		self.__spell = val
 
 	# TODO: Écrire les getter/setter pour la propriété `spell`.
 	#       On peut affecter None.
@@ -65,13 +81,17 @@ class Magician:
 			# Retourner le résultat du calcul de dégâts magiques
 		# Sinon
 			# Retourner le résultat du calcul de dégâts physiques
-		pass
+		if self.will_use_magic():
+			self.__mp -= self.__spell.mp_cost
+			return self._compute_magical_damage(other)
+		else:
+			return self._compute_physical_damage(other)
 
 	def will_use_spell(self):
-		pass
+		return self.using_magic and self.__spell is not None and self.__mp >= self.__spell.mp_cost
 
 	def _compute_magical_damage(self, other):
-		pass
+		Character.compute_damage_output(self.level + self.magic_attack,self.__spell.power,1,1,1/8,(0.85,1))
 
 	def _compute_physical_damage(self, other):
 		# TODO: Calculer le dommage physique exactement de la même façon que dans `Character`
